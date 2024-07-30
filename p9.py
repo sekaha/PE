c545baa91d8a41b359497964c652bf98a4f4f5de
Sekaha <davidsommerfield.zg@gmail.com>
2024-07-29 23:55:38 -0500

    p9 pe

--- p9.py
+++ p9.py
@@ -1,2 +1,15 @@
 # search for integer partitions where
 # a+b+c = 1000, a, b < c... a+b <= c
+import math
+
+
+def get_val(n):
+    for a in range(1, n):
+        for b in range(a + 1, n):
+            c = n - a - b
+
+            if math.sqrt(a**2 + b**2) == c:
+                return a * b * c
+
+
+print(get_val(1000))
